817c1928443c82c8e851f2bd740d611069584188
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-04-24 17:15:27 +0530

    stories writting

--- src/stories/components/RevenueCard.stories.jsx
+++ src/stories/components/RevenueCard.stories.jsx
@@ -1,1 +1,23 @@
 //will write stories for our components so that we can make specific component open source 
+import { RevenueCard } from "../../components/RevenueCard";
+
+const meta = {
+    components: RevenueCard
+};
+export default meta;
+//we can decide for different conditions our UI will appear differently 
+export const BigAmount = {
+    args: {
+        orderCount: "124",
+        title: "Asd",
+        amount: "1000000000"
+    }
+};
+
+export const SmallAmount = {
+    args: {
+        orderCount: "124",
+        title: "Asd",
+        amount: "100"
+    }
+}
